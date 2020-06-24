d468a88b95f0bba7dca5e1ec5a9b6453cc364208
Maryia Sanets <sanetsmary@gmail.com>
2020-06-24 12:33:22 +0300

    branch release commit 1

--- src/Hello.js
+++ src/Hello.js
@@ -4,6 +4,7 @@ function hello() {
   console.log('branch feature1 commit 1');
   console.log('branch feature1 commit 2');
   console.log('branch develop, commit 4');
+  console.log('branch release, commit 1');
 }
 
 hello();
